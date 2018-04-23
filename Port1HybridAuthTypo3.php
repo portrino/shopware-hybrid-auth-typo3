98b82c5731fed869ea19e053f7f5c7d6009ac8f4
Axel Böswetter <boeswetter@portrino.de>
2018-04-23 15:32:53 +0200

    [TASK] updates bootstrap -> move subscriber parts into subscriber classes [TASK] use "mindplay/composer-locator" package to locate other composer packages to activate hybridauth-typo3 provider during plugin install

--- Port1HybridAuthTypo3.php
+++ Port1HybridAuthTypo3.php
@@ -8,16 +8,12 @@ namespace Port1HybridAuthTypo3;
  * Written by (c) Axel Boeswetter <boeswetter@portrino.de>, portrino GmbH
  */
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Port1HybridAuth\Service\AbstractAuthenticationService;
-use Port1HybridAuth\Service\SingleSignOnService;
-use Port1HybridAuthTypo3\Service\AuthenticationService\Typo3AuthenticationService;
+use ComposerLocator;
 use Shopware\Bundle\AttributeBundle\Service\CrudService;
 use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\ActivateContext;
 use Shopware\Components\Plugin\Context\InstallContext;
 use Shopware\Components\Plugin\Context\UninstallContext;
-use Shopware\Components\Theme\LessDefinition;
 use Shopware\Models\Customer\Customer;
 
 /**
@@ -35,7 +31,7 @@ class Port1HybridAuthTypo3 extends Plugin
     public function install(InstallContext $context)
     {
         $this->addIdentityFieldsToUser();
-        $this->copyHybridAuthTypo3Provider();
+        $this->activateHybridAuthTypo3Provider();
 
         parent::install($context);
     }
@@ -47,7 +43,7 @@ class Port1HybridAuthTypo3 extends Plugin
     public function activate(ActivateContext $context)
     {
         $this->addIdentityFieldsToUser();
-        $this->copyHybridAuthTypo3Provider();
+        $this->activateHybridAuthTypo3Provider();
 
         parent::activate($context);
     }
@@ -61,112 +57,10 @@ class Port1HybridAuthTypo3 extends Plugin
     }
 
     /**
-     * @return array
-     */
-    public static function getSubscribedEvents()
-    {
-        return [
-            'Enlight_Controller_Action_PostDispatch_Frontend' => 'onFrontendPostDispatch',
-            'Theme_Compiler_Collect_Plugin_Less' => 'onCollectLessFiles'
-        ];
-    }
-
-    /**
-     * @param \Enlight_Event_EventArgs $args
-     * @throws \Exception
-     */
-    public function onFrontendPostDispatch(\Enlight_Event_EventArgs $args)
-    {
-        /** @var \Enlight_Controller_Action $controller */
-        $controller = $args->get('subject');
-        /** @var \Enlight_View_Default $view */
-        $view = $controller->View();
-
-        /** @var array $config Plugin configuration for current active shop in frontend */
-        $config = Shopware()->Container()->get('shopware.plugin.cached_config_reader')->getByPluginName(
-            'Port1HybridAuthTypo3',
-            $this->container->get('Shop')
-        );
-        $view->assign('Port1HybridAuthTypo3Config', $config);
-
-        if ((bool)$config['general_force_sso'] === true) {
-            /** @var \Enlight_Controller_Action $controller */
-            $controller = $args->get('subject');
-
-            $view = $controller->View();
-            $view->addTemplateDir($this->getPath() . '/Resources/views');
-
-            $controllerName = $controller->Front()->Request()->getControllerName();
-            $actionName = $controller->Front()->Request()->getActionName();
-
-            /**
-             * do not authenticate when user presses logout or an forbidden redirect form auth before takes place!
-             */
-            if (!($controllerName === 'account' && $actionName === 'logout') &&
-                !($controllerName === 'typo3login' && $actionName === 'forbidden') &&
-                !($controllerName === 'typo3login' && $actionName === 'logout')
-            ) {
-
-                /** @var SingleSignOnService $singleSignOnService */
-                $singleSignOnService = $this->container->get('port1_hybrid_auth.single_sign_on_service');
-                $userIsLoggedIn = $singleSignOnService->loginAndRegisterVia('typo3');
-
-                if ((bool)$userIsLoggedIn === false) {
-                    $controller->forward('forbidden', 'typo3login');
-                }
-            }
-
-            /**
-             * do trigger sso logout
-             */
-            if ($controllerName === 'account' && $actionName === 'logout') {
-                /** @var Typo3AuthenticationService $typo3AuthenticationService */
-                $typo3AuthenticationService = $this->container->get('port1_hybrid_auth_typo3.typo3_authentication_service');
-
-                if ($typo3AuthenticationService != null && $typo3AuthenticationService instanceof AbstractAuthenticationService) {
-                    $typo3AuthenticationService->logout();
-                }
-
-                $controller->redirect(
-                    [
-                        'controller' => 'typo3login',
-                        'action' => 'logout'
-                    ]
-                );
-            }
-        }
-    }
-
-    /**
-     * @param \Enlight_Event_EventArgs $args
-     *
-     * @return ArrayCollection
-     */
-    public function onCollectLessFiles(\Enlight_Event_EventArgs $args)
-    {
-        $lessDir = $this->getPath() . '/Resources/views/frontend/_public/src/less/';
-        $lessDefinition = new LessDefinition(
-        // less configuration variables
-            [],
-
-            // less files which should be compiled
-            [
-                $lessDir . 'hybrid_auth.less'
-            ],
-
-            //import directory for less @import commands
-            $lessDir
-        );
-
-        return new ArrayCollection([$lessDefinition]);
-    }
-
-    /**
      * @throws \Exception
      */
     private function addIdentityFieldsToUser()
     {
-
         /** @var CrudService $service */
         $service = $this->container->get('shopware_attribute.crud_service');
 
@@ -193,44 +87,52 @@ class Port1HybridAuthTypo3 extends Plugin
     /**
      * @throws \Exception
      */
-    private function copyHybridAuthTypo3Provider()
+    private function activateHybridAuthTypo3Provider()
     {
         /** @var Port1HybridAuthTypo3 $port1HybridAuth */
-        $port1HybridAuth = Shopware()->Container()->get('kernel')->getPlugins()['Port1HybridAuth'];
+        $port1HybridAuth = $this->container->get('kernel')->getPlugins()['Port1HybridAuth'];
         if ($port1HybridAuth !== null) {
-            $this->copy_recursive(
-                $this->getPath() . '/vendor/hybridauth-additional-providers/hybridauth-typo3/',
-                $this->getVendorPath() . '/hybridauth/hybridauth/hybridauth/Hybrid/'
-            );
+            if (ComposerLocator::isInstalled('hybridauth/hybridauth')) {
+                $hybridauthRootPath = ComposerLocator::getPath('hybridauth/hybridauth');
+                $hybridauthHybridPath = sprintf(
+                    '%1$s%2$shybridauth%2$sHybrid',
+                    rtrim($hybridauthRootPath, \DIRECTORY_SEPARATOR),
+                    \DIRECTORY_SEPARATOR
+                );
+                $hybridauthTypo3Path = sprintf(
+                    '%1$s%2$svendor%2$shybridauth-additional-providers%2$shybridauth-typo3',
+                    rtrim(__DIR__, \DIRECTORY_SEPARATOR),
+                    \DIRECTORY_SEPARATOR
+                );
+            }
+
+            if (
+                file_exists($hybridauthHybridPath) && is_dir($hybridauthHybridPath)
+                    && file_exists($hybridauthTypo3Path) && is_dir($hybridauthTypo3Path)
+            ) {
+                $source = $hybridauthTypo3Path;
+                $dest = $hybridauthHybridPath;
+
+                /** @var \RecursiveDirectoryIterator $dirIterator */
+                $dirIterator = new \RecursiveIteratorIterator(
+                    new \RecursiveDirectoryIterator($source, \RecursiveDirectoryIterator::SKIP_DOTS),
+                    \RecursiveIteratorIterator::SELF_FIRST
+                );
+                foreach ($dirIterator as $item) {
+                    $subItem = $dest . DIRECTORY_SEPARATOR . $dirIterator->getSubPathName();
+                    if ($item->isDir()) {
+                        if (!file_exists($subItem)) {
+                            if (!mkdir($subItem) && !is_dir($subItem)) {
+                                throw new \RuntimeException(sprintf('Directory "%s" was not created', $subItem));
+                            }
+                        }
+                    } else {
+                        copy($item, $subItem);
+                    }
+                }
+            }
         } else {
             throw new \Exception('Please install plugin Port1HybridAuth first!', 1497531163);
         }
     }
-
-    private function getVendorPath()
-    {
-        $reflection = new \ReflectionClass(\Composer\Autoload\ClassLoader::class);
-        return dirname(dirname($reflection->getFileName()));
-    }
-
-    /**
-     * @param string $src
-     * @param string $dst
-     */
-    private function copy_recursive($src, $dst)
-    {
-        $dir = opendir($src);
-        @mkdir($dst);
-        while (($file = readdir($dir)) !== false) {
-            if (($file != '.') && ($file != '..')) {
-                if (is_dir($src . DIRECTORY_SEPARATOR . $file)) {
-                    $this->copy_recursive($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file);
-                } else {
-                    copy($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file);
-                }
-            }
-        }
-        closedir($dir);
-    }
-
 }
